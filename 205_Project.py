af9dd653660dbe42c3658e0d75020182ee8b1a2d
dfaijlan <DFajilan@csumb.edu>
2017-12-07 15:35:58 -0800

    Fixed some issues with program, added a readme file, added more songs

--- 205_Project.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from pygame import mixer
-pygame.mixer.init()
-pygame.init()
-pygame.mixer.music.load('Track01.mp3')
-pygame.mixer.music.set_endevent(pygame.USEREVENT)
-pygame.event.set_allowed(pygame.USEREVENT)
-pygame.mixer.music.play()
-pygame.event.wait()
